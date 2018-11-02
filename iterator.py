687e79e7531c6e965b9b3de3b490ee41b5e64c0c
ewertonP <ewerton.paulo@dce.ufpb.br>
2018-11-02 18:19:11 -0300

    fixes

--- iterator.py
+++ iterator.py
@@ -1,22 +1,49 @@
+
 from grafo import grafo
+import texttable as tt
+
+tab = tt.Texttable()
            
 def visits(grafo, entry, key):
     for vertex in grafo[key]:
         if vertex != 'exit' and vertex != key:    
             try:
-                entry[vertex] = grafo[key][vertex]*entry[key] 
-                visits(grafo, entry, vertex)
+                entry[vertex] += grafo[key][vertex]*entry[key] 
+                visits(grafo,entry,vertex)
             except KeyError:
                 entry[vertex] = grafo[key][vertex]*entry[key] 
-                visits(grafo, entry, vertex)
+                visits(grafo,entry,vertex)
         if vertex == key:
             entry[vertex] = entry[vertex]/(1-grafo[vertex][vertex])
+    return entry
 
-entry = {'entry': 1.0}
+def table(headings,keys):
+    tab = tt.Texttable()
+    tab.header(headings)
+    tab.add_row(keys)
+    s = tab.draw()
+    print (s)
 
 def show(entry):
-    visits(grafo, entry, 'entry')
+    entry = visits(grafo,entry,'entry')
+    headings = []
+    keys = []
     for key in entry:
-        print(key+' %s' %entry[key])
+        headings.append(key)
+        keys.append(entry[key])
 
+    table(headings, keys)
+
+def arrive(entry,taxa):
+    headings = []
+    keys = []
+    for vertex in entry:
+        entry[vertex] = entry[vertex]*taxa
+        headings.append(vertex)
+        keys.append(entry[vertex])
+    table(headings, keys)
+    return entry
+
+entry = {'entry': 1.0}
 show(entry)
+arrive(entry,5)
